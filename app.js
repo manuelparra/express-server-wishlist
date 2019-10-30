7eef411876801bc4382a6c8535c09cea52744557
Manuel Parra <manuelparra@live.com.ar>
2019-10-29 21:23:09 -0400

    NodeJS para desarrollo de APIs basicas sin persistencias

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-import express from "express";
+var express = require("express");
 var app = express();
 app.use(express.json());
 app.listen(3000, () => console.log("Server running on port 3000"));
 
-app.get("/url", (req, res, next) => res.json(misDestinos));
+app.get("/url", (req, res, next) => res.json([ "Paris", "Barcelona", "Barranquilla", "Montevideo", "Santiago de Chile", "Caracas", "Nueva York" ]));
+
+var misDestinos = [];
+app.get("/my", (req, res, next) => res.json(misDestinos));
 app.post("/my", (req, res, next) => {
   console.log(res.body);
   misDestinos = req.body;
